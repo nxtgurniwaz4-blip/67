29d60bd9b5c9181e8745c40c92169d967e371368
nxtgurniwaz4-blip <nxtgurniwaz4@gmail.com>
2026-08-09 22:14:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const http = require('http');
+http.createServer((req, res) => res.end('Bot is running!')).listen(process.env.PORT || 3000);
 "use strict";
 
 const { addLog, getLogs } = require("./logger");
