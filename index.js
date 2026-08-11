38c81741bc14706364198a6c71da54a5010ee342
nxtgurniwaz4-blip <nxtgurniwaz4@gmail.com>
2026-08-11 20:04:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function startBot() {
             console.log('Sent keep-alive command to prevent Limbo sleep.');
         }
     }), 240000);
-  });
+
     
     // INFINITE WALK MODULE: Forces the bot forward constantly
     clearInterval(walkInterval);
@@ -93,7 +93,7 @@ function startBot() {
         bot.setControlState("forward", true);
       }
     }, 5000);
-  });
+
 
   bot.on("end", (reason) => {
     botState.connected = false;
